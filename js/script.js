fefdc785ad58be5fab7662ae3f58d3886ad7266c
twiglj1130 <twig_lj_1130@yahoo.co.jp>
2024-09-07 13:36:37 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,20 +1,38 @@
-$(function () {
+// DOM読み込み完了後に実行
+$(function() {
+  const $header = $('.header');
+  const $pageTop = $('#js-page-top');
+  const navHeight = $header.outerHeight();
 
-  //ページ内スクロール
-  var navHeight = $(".header").outerHeight();
+  // ページ内スクロール
+  $('a[href^="#"]').on('click', function(e) {
+      e.preventDefault();
+      const href = $(this).attr('href');
+      const $target = $(href === '#' || href === '' ? 'html' : href);
+      const position = $target.offset().top - navHeight;
 
-  $('a[href^="#"]').on("click", function () {
-    var href = $(this).attr("href");
-    var target = $(href == "#" || href == "" ? "html" : href);
-    var position = target.offset().top - navHeight;
-    $("html, body").animate({ scrollTop: position, }, 300, "swing");
-    return false;
+      $('html, body').animate({ scrollTop: position }, 300, 'swing');
   });
 
-  //ページトップ
-  $("#js-page-top").on("click", function () {
-    $("body,html").animate({ scrollTop: 0, }, 300);
-    return false;
+  // ページトップへのスクロール
+  $pageTop.on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: 0 }, 300);
   });
 
+  // ページ読み込み時にファビコンを設定
+  setFavicon('./img/favicon.ico');
 });
+
+// ファビコンを設定する関数
+function setFavicon(faviconUrl) {
+  let link = document.querySelector("link[rel~='icon']");
+  
+  if (!link) {
+      link = document.createElement('link');
+      link.rel = 'icon';
+      document.head.appendChild(link);
+  }
+  
+  link.href = faviconUrl;
+}
